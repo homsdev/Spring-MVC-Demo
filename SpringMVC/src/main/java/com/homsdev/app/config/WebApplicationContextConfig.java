dd35546022d6c191a85681c29ac4b737311dc3e8
HomS <obr_dev@protonmail.com>
2021-02-17 12:20:44 -0600

    URI Template Matrix Params
    implemented brand and category filter

--- SpringMVC/src/main/java/com/homsdev/app/config/WebApplicationContextConfig.java
+++ SpringMVC/src/main/java/com/homsdev/app/config/WebApplicationContextConfig.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.util.UrlPathHelper;
 
 @Configuration
 @EnableWebMvc
@@ -20,6 +22,12 @@ public class WebApplicationContextConfig extends WebMvcConfigurerAdapter {
 			configurer.enable();
 		}
 
+		@Override
+		public void configurePathMatch(PathMatchConfigurer configurer) {
+			UrlPathHelper urlPathHelper=new UrlPathHelper();
+			urlPathHelper.setRemoveSemicolonContent(false);
+			configurer.setUrlPathHelper(urlPathHelper);
+		}
 		
 //		This bean resolves the actual view´s file path
 //      Its job is to form the actual URL from the returned String from the controller
--- SpringMVC/src/main/java/com/homsdev/app/controller/ProductController.java
+++ SpringMVC/src/main/java/com/homsdev/app/controller/ProductController.java
@@ -1,8 +1,12 @@
 package com.homsdev.app.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -39,8 +43,8 @@ public class ProductController {
 	}
 	
 	@RequestMapping("/products/filter/{params}")
-	public String getProductsByFilter(Model model, @PathVariable("params") Map<String,List<String>> paramsFilter) {
-		
+	public String getProductsByFilter(Model model,@MatrixVariable(pathVar = "params") Map<String,List<String>>filterParams) {
+		model.addAttribute("products", productService.getProductByFilter(filterParams));
 		return "products";
 	}
 }
