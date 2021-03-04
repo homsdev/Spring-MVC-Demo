b398bd159ba5d042ad5c02a0135962f64abcb4d0
HomS <obr_dev@protonmail.com>
2021-03-04 13:44:49 -0600

    Implemented JSON view resolver

--- SpringMVC/src/main/java/com/homsdev/app/config/WebApplicationContextConfig.java
+++ SpringMVC/src/main/java/com/homsdev/app/config/WebApplicationContextConfig.java
@@ -1,18 +1,26 @@
 package com.homsdev.app.config;
 
+import java.util.ArrayList;
+
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 import org.springframework.web.util.UrlPathHelper;
+
 
 @Configuration
 @EnableWebMvc
@@ -56,4 +64,25 @@ public class WebApplicationContextConfig extends WebMvcConfigurerAdapter {
 		resource.setBasename("messages");
 		return resource;
 	}
+	
+	//Configuring JSON view
+	@Bean
+	public MappingJackson2JsonView jsonView() {
+		MappingJackson2JsonView jsonView=new MappingJackson2JsonView();
+		jsonView.setPrettyPrint(true);
+		return jsonView;
+	}
+	
+	
+	//configuring Content negotiation view resolver
+	@Bean
+	public ViewResolver negotiationViewResolver(ContentNegotiationManager contentManager) {
+		ContentNegotiatingViewResolver resolver=new ContentNegotiatingViewResolver();
+		resolver.setContentNegotiationManager(contentManager);
+		ArrayList<View> views=new ArrayList<View>();
+		views.add(jsonView());
+		resolver.setDefaultViews(views);
+		return resolver;
+	}
+	
 }
