133c14aa70590f50253c395c7dec7ab1082271c5
HomS <obr_dev@protonmail.com>
2021-03-03 23:23:24 -0600

    Changes in home controller
    -Aplied fowarded url to /welcome/greetings

--- SpringMVC/src/main/java/com/homsdev/app/controller/HomeController.java
+++ SpringMVC/src/main/java/com/homsdev/app/controller/HomeController.java
@@ -5,13 +5,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
 	
-	@RequestMapping()
+	@RequestMapping("/")
 	public String welcome(Model model) {
 		model.addAttribute("greeting", "Welcome to My First Spring MVC App");
 		model.addAttribute("tagline","The one and only best Web App");
+		return "redirect:/welcome/greeting";
+	}
+	
+	@RequestMapping("/welcome/greeting")
+	public String greetin(Model model) {
 		return "welcome";
 	}
 }
